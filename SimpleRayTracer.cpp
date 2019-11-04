4de2cbf9681d6d0e82efb15c856862872a1ff1a1
melonehco <mzmao@colby.edu>
2019-11-03 22:57:16 -0800

    added random_scene to main file. (ch 12) finished

--- SimpleRayTracer.cpp
+++ SimpleRayTracer.cpp
@@ -50,6 +50,52 @@ vec3 color(const ray& r, hitable *world, int depth)
     //linear interpolation (gradient) between white and blue
 }
 
+hitable *random_scene()
+{
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+
+    //the floor
+    list[0] = new sphere( vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)) );
+
+    int i = 1;
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            float choose_mat = drand48();
+            vec3 center(a + 0.9*drand48(), 0.2, b + 0.9*drand48());
+
+            //space away from big spheres, I think?
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) 
+            {
+                if (choose_mat < 0.8) //diffuse
+                {
+                    list[i++] = new sphere(center, 0.2, new lambertian( vec3(drand48()*drand48(),
+                                                                             drand48()*drand48(),
+                                                                             drand48()*drand48()) ));
+                }
+                else if (choose_mat < 0.95) //metal
+                {
+                    list[i++] = new sphere( center, 0.2,
+                                            new metal( vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48() ) );
+                }
+                else //glass
+                {
+                    list[i++] = new sphere( center, 0.2, new dielectric(1.5) );
+                }
+            }
+        }
+    }
+
+    //three big spheres
+    list[i++] = new sphere( vec3(0, 1, 0), 1.0, new dielectric(1.5) );
+    list[i++] = new sphere( vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)) );
+    list[i++] = new sphere( vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0) );
+
+    return new hitable_list(list, i);
+}
+
 int main(int argc, char **argv)
 {
     int width = 200;
@@ -67,27 +113,29 @@ int main(int argc, char **argv)
     }
     imgfile << "P3\n" << width << " " << height << "\n255\n";
 
-    vec3 lookfrom(3, 3, 2);
+    vec3 lookfrom(4.2, 2, 3);
     vec3 lookat(0, 0, -1);
     float dist_to_focus = (lookat - lookfrom).length();
-    float aperture = 2.0;
+    float aperture = 0.1;
     //         look from, look at, vup          vfov, aspect ratio
-    camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(width)/float(height), aperture, dist_to_focus);
+    camera cam(lookfrom, lookat, vec3(0, 1, 0), 90, float(width)/float(height), aperture, dist_to_focus);
 
-    hitable *list[4];
+    //hitable *list[4];
     //TODO: what's the difference btw calling with and w/o new?
     //----------------------version up to ch 9
-    list[0] = new sphere( vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)) );
-    list[1] = new sphere( vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)) ); //basically the floor
-    list[2] = new sphere( vec3(1, 0, -1), 0.5, new dielectric(1.5) );
+    // list[0] = new sphere( vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)) );
+    // list[1] = new sphere( vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)) ); //basically the floor
+    // list[2] = new sphere( vec3(1, 0, -1), 0.5, new dielectric(1.5) );
     //list[3] = new sphere( vec3(1, 0, -1), -0.45, new dielectric(1.5) ); //inner sphere for bubble
-    list[3] = new sphere( vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.2) );
+    // list[3] = new sphere( vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.2) );
     //----------------------version for ch 10 pt 1
     // float R = cos(M_PI/4.0);
     // list[0] = new sphere( vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)) );
     // list[1] = new sphere( vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)) );
     //----------------------
-    hitable *world = new hitable_list(list, 4);
+    //hitable *world = new hitable_list(list, 4);
+
+    hitable *world = random_scene();
 
     for (int j = height - 1; j >= 0; j--)
     {
